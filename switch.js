241f4bc812e8f8155e49cb708373d480af3f3754
NajafAbbas <145696441+NajafAbbas@users.noreply.github.com>
2024-08-28 00:09:58 -0700

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,44 @@
+// const day = 6;
+// if (day == 1) {
+//     console.log("Sunday");
+// } else if (day == 2) {
+//     console.log("Monday");
+// } else if (day == 3) {
+//     console.log("Tuesday");
+// } else if (day == 4) {
+//     console.log("wednessday");
+// } else if (day == 5) {
+//     console.log("Thursday");
+// } else if (day == 6) {
+//     console.log("Friday");
+// } else if (day == 7) {
+//     console.log("Saturday");
+// } else {
+//     console.log("Invalid syntax please try again ");
+// }
+const day = 5;
+switch (day) {
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("Wednessday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("Saturday")
+        break;
+    case 1:
+        console.log("Sunday")
+        break;
+    default:
+        console.log("Invalid syntax please try again later")
+}
